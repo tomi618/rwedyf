7b398fb0ace6a1f1a03363e652dd34afc8f15b57
agunbiade tomi <agunbiadetomi618@gmail.com>
2025-04-23 17:16:13 +0100

    changed the database uri

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from rwydef import create_app
 from init_db import init_db
 from rwydef.extensions import db
+import os
 
 app = create_app()
+init_db(app, db)
 
 if __name__ == '__main__':
-      init_db(app, db)
-      app.run()
+      port = int(os.environ.get("PORT", 5000))
+      app.run(host="0.0.0.0", port=port)

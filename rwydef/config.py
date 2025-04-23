42ea48d9ac0b56da67f21e5d8de46275d6d157b3
agunbiade tomi <agunbiadetomi618@gmail.com>
2025-04-23 18:15:22 +0100

    set PORT in config.py to get from environment variable

--- rwydef/config.py
+++ rwydef/config.py
@@ -7,5 +7,6 @@ load_dotenv(os.path.join(os.getcwd(), 'rwydef', '.env'))
 password = quote_plus(os.getenv('PASSWORD'))
 class Config():
       DEBUG=False
+      PORT=os.getenv('PORT')
       SECRET_KEY=os.getenv('SECRET_KEY')
       SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')#f"postgresql+psycopg2://{os.getenv('USER')}:{password}@localhost:{os.getenv('PORT')}/{os.getenv('DB')}" 
